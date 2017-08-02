8cf619926287fa9566bedb5e933c2de110c84ad9
ruthbuena <ruthbpeace@gmail.com>
2017-08-02 16:10:26 -0400

    Updates to README file to explain what application in this repo is used for

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Bamazon
-Node.js &amp; MySQL
+Bamazon
+
+In this Repository, users have the ability to create and maintain a marketplace using Node.js and MySQL.
+
+In Part I, a database (bamazon) has been created using MySQL to keep track of inventory.  The Id number, product name, department name, price and stock quantity for each product will be maintained in the bamazon database.  Node application (bamazonCustomer.js) has been created to allow customers to purchase items based on availability.  The bamazon database will be updated after purchases are completed.
--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -13,5 +13,9 @@ CREATE TABLE products (
   PRIMARY KEY (id)
 );
 
+
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("The Late Show", "Books", 12.99, 10),("The Whistler", "Books", 9.99, 10),("Camino Island", "Books", 11.99, 10);
+VALUES ("The Late Show", "Books", 12.99, 10),("The Whistler", "Books", 9.99, 10),("Camino Island", "Books", 11.99, 10),
+("House of Spies", "Books", 10.99, 10),("Beautiful Mistake", "Books", 14.99, 10),("The Woman in Cabin 10", "Books", 13.99, 10),
+("A Game of Thrones", "Books", 15.99, 10),("The Handmaid's Tale", "Books", 14.99, 10),("The Gunslinger", "Books", 12.99, 10),
+("Milk and Honey", "Books", 16.99, 10);
